ab0c23df608cb488288f7ec7f7f0eaa460c8b390
FedericoCalonge <calongefederico@gmail.com>
2019-12-10 08:53:57 -0300

    tests domgoles modificado

--- TP1/src/test/java/dom/DOMGolesTest.java
+++ TP1/src/test/java/dom/DOMGolesTest.java
@@ -7,63 +7,100 @@ import java.util.Map;
 public class DOMGolesTest {
 
     @Test
-    public void devolverVisitante3GolesACauteruccio() throws Exception {
+    public void golesVisitanteYLocales() throws Exception {
+        //Escenario: Quilmes 6 (Cauteruccio x 3, Rescaldani x 2, Mandarino) - Brown 2 (Aciar y Vera)
         DOMGoles domGoles = new DOMGoles();
         String filename = "quilmes_2012_muchos_goles.xml";
 
-        Map<String, Integer> DicGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
-        int cantidadGolesDic = DicGolesVisitante.get("Martín Cauteruccio");
+        Map<String, Integer> diccionarioGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
+        Map<String, Integer> diccionarioGolesLocal = domGoles.dameDiccionarioAutorCantidadGoles("local", filename);
 
-        Assert.assertEquals("El metodo devolverVisitante3GolesACauteruccio no esta devolviendo el valor esperado",
-                cantidadGolesDic, 3);
+        int cantidadGolesDicMandarino = diccionarioGolesVisitante.get("Germán Mandarino");
+        int cantidadGolesDicRescaldani = diccionarioGolesVisitante.get("Ezequiel Rescaldani");
+        int cantidadGolesDicCauteruccio = diccionarioGolesVisitante.get("Martín Cauteruccio");
+
+        int cantidadGolesDicAciar= diccionarioGolesLocal.get("Walter Aciar");
+        int cantidadGolesDicVera= diccionarioGolesLocal.get("Mario Vera");
+
+        int autoresDistintosDeGolesVisitante = 3;
+        int autoresDistintosDeGolesLocal= 2;
+
+        //Cantidad de goles:
+        Assert.assertEquals("Hay 3 autores distintos de goles para el visitante y eso no se refleja en el diccionario", autoresDistintosDeGolesVisitante,
+                diccionarioGolesVisitante.size());
+        Assert.assertEquals("Hay 2 autores distintos de goles para el local y eso no se refleja en el diccionario", autoresDistintosDeGolesLocal,
+                diccionarioGolesLocal.size());
+
+        //Goles visitantes:
+        Assert.assertEquals("La cantidad de goles de Mandarino es incorrecta",
+                cantidadGolesDicMandarino, 1);
+        Assert.assertEquals("La cantidad de goles de Rescaldani es incorrecta",
+                cantidadGolesDicRescaldani, 2);
+        Assert.assertEquals("La cantidad de goles de Cauteruccio es incorrecta",
+                cantidadGolesDicCauteruccio, 3);
+
+        //Goles locales:
+        Assert.assertEquals("La cantidad de goles de Aciar es incorrecta",
+                cantidadGolesDicAciar, 1);
+        Assert.assertEquals("La cantidad de goles de Vera es incorrecta",
+                cantidadGolesDicVera, 1);
     }
 
     @Test
-    public void devolverVisitante2GolesARescaldani() throws Exception {
+    public void devolver1GolVisitate0Local() throws Exception {
+        //Escenario: Quilmes 1 (Rescaldani) - Brown 0.
         DOMGoles domGoles = new DOMGoles();
-        String filename = "quilmes_2012_muchos_goles.xml";
+        String filename = "quilmes_2012_1_gol_vis.xml";
 
-        Map<String, Integer> DicGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
-        int cantidadGolesDic = DicGolesVisitante.get("Ezequiel Rescaldani");
+        Map<String, Integer> diccionarioGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
+        Map<String, Integer> diccionarioGolesLocal = domGoles.dameDiccionarioAutorCantidadGoles("local", filename);
 
-        Assert.assertEquals("El metodo devolverVisitante2GolesARescaldani no esta devolviendo el valor esperado",
-                cantidadGolesDic, 2);
+        int cantidadGolesDicRescaldani = diccionarioGolesVisitante.get("Ezequiel Rescaldani");
+
+        //Cantidad de goles:
+        Assert.assertEquals("La cantidad de goles para el visitante no es la correcta",
+                diccionarioGolesVisitante.size(), 1);
+        Assert.assertEquals("La cantidad de goles para el local no es la correcta",
+                diccionarioGolesLocal.size(), 0);
+
+        //Gol visitante:
+        Assert.assertEquals("La cantidad de goles de Rescaldani es incorrecta",
+                cantidadGolesDicRescaldani, 1);
     }
 
     @Test
-    public void devolverVisitante1GolAMandarino() throws Exception {
+    public void devolver1GolLocal0Visitante() throws Exception {
+        //Escenario: Quilmes 0 - Brown 1 (Aciar).
         DOMGoles domGoles = new DOMGoles();
-        String filename = "quilmes_2012_muchos_goles.xml";
+        String filename = "quilmes_2012_1_gol_local.xml";
 
-        Map<String, Integer> DicGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
-        int cantidadGolesDic = DicGolesVisitante.get("Germán Mandarino");
+        Map<String, Integer> diccionarioGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
+        Map<String, Integer> diccionarioGolesLocal = domGoles.dameDiccionarioAutorCantidadGoles("local", filename);
 
-        Assert.assertEquals("El metodo devolverVisitante1GolAMandarino no esta devolviendo el valor esperado",
-                cantidadGolesDic, 1);
+        int cantidadGolesDicAciar = diccionarioGolesLocal.get("Walter Aciar");
+
+        //Cantidad de goles:
+        Assert.assertEquals("La cantidad de goles para el visitante no es la correcta",
+                diccionarioGolesVisitante.size(), 0);
+        Assert.assertEquals("La cantidad de goles para el local no es la correcta",
+                diccionarioGolesLocal.size(), 1);
+
+        //Gol local:
+        Assert.assertEquals("La cantidad de goles de Aciar es incorrecta",
+                cantidadGolesDicAciar, 1);
     }
 
     @Test
-    public void devolverLocal1GolAAciar() throws Exception {
-        DOMGoles domGoles = new DOMGoles();
-        String filename = "quilmes_2012_muchos_goles.xml";
-        Map<String, Integer> DicGolesLocal = domGoles.dameDiccionarioAutorCantidadGoles("local", filename);
-        int cantidadGolesDic = DicGolesLocal.get("Walter Aciar");
-
-        Assert.assertEquals("El metodo devolverLocal1GolAAciar no esta devolviendo el valor esperado",
-                cantidadGolesDic, 1);
-    }
-
-    @Test
-    public void devolver0Goles() throws Exception {
+    public void devolver0GolesVisYLocal() throws Exception {
         DOMGoles domGoles = new DOMGoles();
         String filename = "quilmes_2012_sin_goles.xml";
 
         Map<String, Integer> DicGolesVisitante = domGoles.dameDiccionarioAutorCantidadGoles("visitante", filename);
         Map<String, Integer> DicGolesLocal = domGoles.dameDiccionarioAutorCantidadGoles("local", filename);
 
+        //Cantidad de goles:
         Assert.assertEquals("El metodo devolver0Goles no esta devolviendo el valor esperado para el local",
                 DicGolesLocal.size(), 0);
-
         Assert.assertEquals("El metodo devolver0Goles no esta devolviendo el valor esperado para el visitante",
                 DicGolesVisitante.size(), 0);
     }
